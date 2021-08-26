2a1de50429b141f31f676aafb74f7776a264ab38
Dovile Tumaite <dovile.tumaite@yahoo.it>
2021-08-26 07:27:52 +0200

    ready to merge

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,18 +2,46 @@ import React from "react";
 import { InputGroup, FormControl, Button } from "react-bootstrap";
 // import books from '../data/books.json'
 
-function Input() {
-  return (
+// class SearchForm extends Component {
+//     state = {
+//         search: {
+//             keyword: '',
+//         }
+//     }
+
+// handleInput = (e, keyValue) => {
+//     this.searchState({
+//         search: {
+//             ...this.state.search, [keyValue]: keyValue == e.target.value
+//         }
+//     })
+// }
+// handleSearch = async (e) => {
+//     e.preventDefault()
+//     try {
+//         let response = await fetch("", {
+//         Method: 'GET',
+//         body: JSON.stringify(this.state.search),
+//         })
+//         headers: {'content-type': 'application/json'}
+//     } catch (error) {
+//         console.log(err)
+//     }
+// }
+//  render(){
+//  
+    function Input () { 
+        return (
     <InputGroup className="mb-3">
       <FormControl
         placeholder="Recipient's username"
         aria-label="Recipient's username"
         aria-describedby="basic-addon2"
-       // value={this.target.value}
-        onChange={(e) => {
-          e.preventDefault();
-          console.log(e.target.value);
-        }}
+      //  value={this.state.search}
+    //    onChange={(e) => {
+     //     e.preventDefault();
+    //      console.log(e.target.value);
+      //  }}
       />
       <Button variant="outline-secondary" id="button-addon2">
         Search
@@ -21,4 +49,5 @@ function Input() {
     </InputGroup>
   );
 }
+
 export default Input;
